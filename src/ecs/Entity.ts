1577d13f0895588704809c05337f3ad3ef16adf4
Justin Cotarla <justin-cotarla@users.noreply.github.com>
2024-07-13 19:02:03 +0000

    add support adding for empty components to entities

--- src/ecs/Entity.ts
+++ src/ecs/Entity.ts
@@ -6,11 +6,12 @@ export class Entity {
 
   constructor(readonly id: number) {}
 
+  addComponent<T>(component: Component<T>): void
   addComponent<T, V extends T>(component: Component<T>, value: V): void
   addComponent<T, V extends T>(component: DefaultComponent<T>, value?: V): void
   addComponent<T, V extends T, C extends DefaultComponent<T> | Component<T>>(
     component: C,
-    value: C extends Component<T> ? V : V | undefined
+    ...[value]: C extends Component<T> ? [value: V] : [value?: V]
   ): void {
     component.addToEntity(this.id, value)
 
